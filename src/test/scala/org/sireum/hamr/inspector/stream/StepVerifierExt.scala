58efa94468385a398223d0c56391c59337205134
Matthew Weis <matthewweiscaps@gmail.com>
2020-02-13 13:34:33 -0600

    updated project packages

--- src/test/scala/org/sireum/hamr/inspector/stream/StepVerifierExt.scala
+++ src/test/scala/org/sireum/hamr/inspector/stream/StepVerifierExt.scala
@@ -1,4 +1,4 @@
-package test
+package org.sireum.hamr.inspector.stream
 
 import java.time.{Duration => JDuration}
 import java.util.function.{Supplier => JSupplier}
@@ -10,32 +10,11 @@ import scala.language.implicitConversions
 
 object StepVerifierExt {
 
-  // pimp my library pattern
-
-//  class StepVerifier_Ext(stepVerifier: StepVerifier) {
-//    def withVirtualTime[T](scenarioSupplier: () => Publisher[T]): StepVerifier.FirstStep[T] = {
-//      StepVerifier.withVirtualTime(scalaToJavaSupplier(scenarioSupplier))
-//    }
-//  }
-
-//  object StepVerifierVirtualTime extends AnyVal {
-//    def withVirtualTime[T](scenarioSupplier: () => Publisher[T]): StepVerifier.FirstStep[T] = {
-//      StepVerifier.withVirtualTime[T](scalaToJavaSupplier(scenarioSupplier))
-//    }
-//  }
-//
-//  implicit def stepVerifier_Ext(stepVerifier: StepVerifier.type): StepVerifierVirtualTime.type = StepVerifierVirtualTime
-
-
-
-
   class StepVerifierStep_Ext[T](stepVerifierStep: StepVerifier.Step[T]) {
     def expectNoEvent(duration: FiniteDuration): reactor.test.StepVerifier.Step[T] = {
       stepVerifierStep.expectNoEvent(scalaToJavaDuration(duration))
     }
   }
-
-//  def scenario(name: String): StepVerifierOptions = StepVerifierOptions.create().scenarioName(name)
 
   implicit def stepVerifierStep_Ext[T](stepVerifierStep: StepVerifier.Step[T]): StepVerifierStep_Ext[T] =
     new StepVerifierStep_Ext[T](stepVerifierStep)
